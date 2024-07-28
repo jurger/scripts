ec0e97a2eb58264b7c43e12be70eb34214a7bf83
Yuriy <freekvant@gmail.com>
2024-07-28 13:36:17 +0300

    Update install-zabbix-agent2.sh
    opns.fortlab.net

--- install-zabbix-agent2.sh
+++ install-zabbix-agent2.sh
@@ -14,9 +14,12 @@ dpkg -i zabbix-release_6.0-5+${OS}${VERSION_ID}_all.deb
 apt update
 apt install -y zabbix-agent2 zabbix-agent2-plugin-*
 
-read -p "Enter IP-address or domain name of zabbix server: " -e ZBX_SERVER
-sed -i 's/Server=127.0.0.1/Server=$ZBX_SERVER/' /etc/zabbix/zabbix_agent2.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=$ZBX_SERVER/' /etc/zabbix/zabbix_agent2.conf
+#read -p "Enter IP-address or domain name of zabbix server: " -e ZBX_SERVER
+#sed -i 's/Server=127.0.0.1/Server=$ZBX_SERVER/' /etc/zabbix/zabbix_agent2.conf
+#sed -i 's/ServerActive=127.0.0.1/ServerActive=$ZBX_SERVER/' /etc/zabbix/zabbix_agent2.conf
+sed -i 's/Server=127.0.0.1/Server=opns.fortlab.net/' /etc/zabbix/zabbix_agent2.conf
+sed -i 's/ServerActive=127.0.0.1/ServerActive=opns.fortlab.net/' /etc/zabbix/zabbix_agent2.conf
+
 systemctl restart zabbix-agent2
 systemctl enable zabbix-agent2
 systemctl restart zabbix-agent2
